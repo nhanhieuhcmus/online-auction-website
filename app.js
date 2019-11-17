01705f187676bc65b2a1e378610ec60e0f45d795
baophuc53 <55937237+baophuc53@users.noreply.github.com>
2019-11-17 20:38:39 +0700

    add handlebars

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ var app = express();
  
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+app.use(express.static('public'))
  
 app.get('/', function (req, res) {
-    res.render('home');
+    res.render('home', {title: 'Home Page'});
 });
 
 app.get('/items', function (req, res) {
-    res.render('items');
+    res.render('items', {title: 'Product Page'});
 });
 app.listen(3000,()=>{
     console.log('Web server running at sfhkfj');
