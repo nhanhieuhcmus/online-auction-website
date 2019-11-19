7ec970dc4f7db9c1596e7cc3af6599ac33339f21
baophuc53 <55937237+baophuc53@users.noreply.github.com>
2019-11-19 15:54:49 +0700

    add profile

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
- 
 var app = express();
- 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.use(express.static('public'))
@@ -14,6 +12,11 @@ app.get('/', function (req, res) {
 app.get('/items', function (req, res) {
     res.render('items', {title: 'Product Page'});
 });
+
+app.get('/profile', function (req, res) {
+    res.render('profile', {title: 'Profile'});
+});
+
 app.listen(3000,()=>{
     console.log('Web server running at sfhkfj');
 })
